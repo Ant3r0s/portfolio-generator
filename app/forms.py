a1aa1929e99f5be7e5cd70af2933df2967ea5b80
Ant3r0s <98061959+Ant3r0s@users.noreply.github.com>
2024-09-11 01:37:59 +0200

    Create forms.py

---
+++ app/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+
+class CVForm(FlaskForm):
+    name = StringField('Nombre', validators=[DataRequired()])
+    profession = StringField('Profesión', validators=[DataRequired()])
+    experience = TextAreaField('Experiencia', validators=[DataRequired()])
+    education = TextAreaField('Educación', validators=[DataRequired()])
+    skills = TextAreaField('Habilidades', validators=[DataRequired()])
+    submit = SubmitField('Generar Portfolio')
